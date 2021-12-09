3574704a2c37ddc1b4f243115bfc5206c1ca9b5f
marckysan <e0425377@u.nus.edu>
2021-12-09 21:53:40 +0800

    feat(enquiryform, meet): enquiryForm ui done

--- src/pages/meet/meet.tsx
+++ src/pages/meet/meet.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import { useInView } from "react-intersection-observer";
 import { SocialIcon } from "react-social-icons";
 import meetImage from "../../assets/images/meetImage.png";
+import EnquiryForm from "../../components/enquiryForm/enquiryForm";
 import Footer from "../../components/footer/footer";
 import Header from "../../components/header/header";
 import "./meet.css";
@@ -34,12 +35,20 @@ const Meet: FC = () => {
         </div>
 
         <div className="rightSection">
-          <iframe
-            title="Map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31909.981076783202!2d103.87620136715918!3d1.327399938038034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da17f56298db39%3A0xd252ca15e4794c87!2sSingapore%20408720!5e0!3m2!1sen!2ssg!4v1638775401977!5m2!1sen!2ssg"
-            className="map"
-            loading="lazy"
-          ></iframe>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+            }}
+          >
+            <iframe
+              title="Map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31909.981076783202!2d103.87620136715918!3d1.327399938038034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da17f56298db39%3A0xd252ca15e4794c87!2sSingapore%20408720!5e0!3m2!1sen!2ssg!4v1638775401977!5m2!1sen!2ssg"
+              className="map"
+              loading="lazy"
+            ></iframe>
+          </div>
           <div className="contact">
             <div className="contactHeader">
               <h3 className="contactHeaderWords">
@@ -47,6 +56,7 @@ const Meet: FC = () => {
                   url="https://www.instagram.com/claystudios_sg/"
                   bgColor="white"
                   fgColor="black"
+                  className="socialMediaIcon"
                 />
                 Let's Meet!
               </h3>
@@ -102,39 +112,13 @@ const Meet: FC = () => {
                 />
                 <div
                   className="fadingRightEffect"
-                  style={{ animationPlayState: inView ? "running" : "paused" }}
+                  style={{
+                    animationPlayState: inView ? "running" : "paused",
+                  }}
                 ></div>
               </div>
             </div>
-            <div className="inputSection">
-              <label>First Name</label>
-              <input style={{ width: "30%" }}></input>
-            </div>
-            <div className="inputSection">
-              <label>Last Name</label>
-              <input style={{ width: "30%" }}></input>
-            </div>
-            <div className="inputSection">
-              <label>Your Email</label>
-              <input style={{ width: "30%" }}></input>
-            </div>
-            <div className="inputSection">
-              <label>Email Subject</label>
-              <input style={{ width: "30%" }}></input>
-            </div>
-            <div className="inputSection">
-              <label>Your Message</label>
-              <textarea style={{ width: "30%" }}></textarea>
-            </div>
-            <div
-              className="inputSection"
-              style={{ width: "30%", flexDirection: "row" }}
-            >
-              <input type="checkbox"></input>
-              By checking this box and submitting your information, you are
-              granting us permission to email you. You may unsubscribe at any
-              time.
-            </div>
+            <EnquiryForm></EnquiryForm>
           </div>
         </div>
       </div>
