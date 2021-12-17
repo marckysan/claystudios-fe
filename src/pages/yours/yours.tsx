502abb58fd40fd68edb98043cef6f31c6ab60805
marckysan <e0425377@u.nus.edu>
2021-12-18 01:15:35 +0800

    feat(yours, projectcard): responsive project cards done

--- src/pages/yours/yours.tsx
+++ src/pages/yours/yours.tsx
@@ -1,12 +1,21 @@
 import React, { FC, useEffect, useState } from "react";
-import yoursImage from "../../assets/images/yoursImage.png";
+import { Col, Container, Row } from "react-grid-system";
+import punggolWalk from "../../assets/images/308PunggolWalk.png";
+import domain21 from "../../assets/images/domain21.png";
+import floralvale from "../../assets/images/floralvale.png";
+import tamanSelamat from "../../assets/images/tamanSelamat.png";
+import vibesAtEastCoast from "../../assets/images/vibesAtEastCoast.png";
 import Footer from "../../components/footer/footer";
 import Header from "../../components/header/header";
 import ProjectCard from "../../components/projectCard/projectCard";
+import useWindowDimensions from "../../utils/viewportDimensions/useWindowDimensions";
 import "./yours.css";
 
 const Yours: FC = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const { width } = useWindowDimensions();
+
+  console.log(width);
 
   useEffect(() => {
     setTimeout(() => {
@@ -72,7 +81,279 @@ const Yours: FC = () => {
           <p className="yoursText">yours</p>
         </div>
         <div className="rightSection">
-          <ProjectCard img={yoursImage} name="Project A"></ProjectCard>
+          <Container fluid style={{ paddingBottom: "3vw" }}>
+            {width > 1000 ? (
+              <div>
+                <Row debug align="center" justify="center" direction="row">
+                  <Col
+                    debug
+                    style={{
+                      outline: "none",
+                      background: "white",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <ProjectCard
+                      img={vibesAtEastCoast}
+                      name="Vibes @ East Coast"
+                      link="/"
+                    ></ProjectCard>
+                  </Col>
+                  <Col
+                    debug
+                    style={{
+                      outline: "none",
+                      background: "white",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <ProjectCard
+                      img={floralvale}
+                      name="The Floralvale"
+                      link="/"
+                    ></ProjectCard>
+                  </Col>
+                  <Col
+                    debug
+                    style={{
+                      outline: "none",
+                      background: "white",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <ProjectCard
+                      img={tamanSelamat}
+                      name="Taman Selamat"
+                      link="/"
+                    ></ProjectCard>
+                  </Col>
+                </Row>
+                <br />
+                <Row debug align="center" justify="center" direction="row">
+                  <Col
+                    debug
+                    style={{
+                      outline: "none",
+                      background: "white",
+                      paddingLeft: "10%",
+                    }}
+                  >
+                    <ProjectCard
+                      img={domain21}
+                      name="Domain 21"
+                      link="/"
+                    ></ProjectCard>
+                  </Col>
+                  <Col
+                    debug
+                    style={{
+                      outline: "none",
+                      background: "white",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <ProjectCard
+                      img={punggolWalk}
+                      name="308B Punggol Walk"
+                      link="/"
+                    ></ProjectCard>
+                  </Col>
+                </Row>
+              </div>
+            ) : width < 1000 && width > 667 ? (
+              <div>
+                <Row debug align="center" justify="center" direction="row">
+                  <Col
+                    debug
+                    style={{
+                      outline: "none",
+                      background: "white",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <ProjectCard
+                      img={vibesAtEastCoast}
+                      name="Vibes @ East Coast"
+                      link="/"
+                    ></ProjectCard>
+                  </Col>
+                  <Col
+                    debug
+                    style={{
+                      outline: "none",
+                      background: "white",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <ProjectCard
+                      img={floralvale}
+                      name="The Floralvale"
+                      link="/"
+                    ></ProjectCard>
+                  </Col>
+                </Row>
+                <br />
+                <Row debug align="center" justify="center" direction="row">
+                  <Col
+                    debug
+                    style={{
+                      outline: "none",
+                      background: "white",
+                    }}
+                  >
+                    <ProjectCard
+                      img={tamanSelamat}
+                      name="Taman Selamat"
+                      link="/"
+                    ></ProjectCard>
+                  </Col>
+                  <Col
+                    debug
+                    style={{
+                      outline: "none",
+                      background: "white",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <ProjectCard
+                      img={domain21}
+                      name="Domain 21"
+                      link="/"
+                    ></ProjectCard>
+                  </Col>
+                </Row>
+                <Row
+                  debug
+                  align="center"
+                  justify="center"
+                  direction="row"
+                  style={{ marginTop: "2vw" }}
+                >
+                  <Col
+                    debug
+                    style={{
+                      outline: "none",
+                      background: "white",
+                      paddingLeft: "30%",
+                    }}
+                  >
+                    <ProjectCard
+                      img={punggolWalk}
+                      name="308B Punggol Walk"
+                      link="/"
+                    ></ProjectCard>
+                  </Col>
+                </Row>
+              </div>
+            ) : (
+              <div>
+                <Row debug align="center" justify="center" direction="row">
+                  <Col
+                    debug
+                    style={{
+                      outline: "none",
+                      background: "white",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <ProjectCard
+                      img={vibesAtEastCoast}
+                      name="Vibes @ East Coast"
+                      link="/"
+                    ></ProjectCard>
+                  </Col>
+                </Row>
+                <Row
+                  debug
+                  align="center"
+                  justify="center"
+                  direction="row"
+                  style={{ marginTop: "2vw" }}
+                >
+                  <Col
+                    debug
+                    style={{
+                      outline: "none",
+                      background: "white",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <ProjectCard
+                      img={floralvale}
+                      name="The Floralvale"
+                      link="/"
+                    ></ProjectCard>
+                  </Col>
+                </Row>
+                <Row
+                  debug
+                  align="center"
+                  justify="center"
+                  direction="row"
+                  style={{ marginTop: "2vw" }}
+                >
+                  <Col
+                    debug
+                    style={{
+                      outline: "none",
+                      background: "white",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <ProjectCard
+                      img={tamanSelamat}
+                      name="Taman Selamat"
+                      link="/"
+                    ></ProjectCard>
+                  </Col>
+                </Row>
+                <Row
+                  debug
+                  align="center"
+                  justify="center"
+                  direction="row"
+                  style={{ marginTop: "2vw" }}
+                >
+                  <Col
+                    debug
+                    style={{
+                      outline: "none",
+                      background: "white",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <ProjectCard
+                      img={domain21}
+                      name="Domain 21"
+                      link="/"
+                    ></ProjectCard>
+                  </Col>
+                </Row>
+                <Row
+                  debug
+                  align="center"
+                  justify="center"
+                  direction="row"
+                  style={{ marginTop: "2vw" }}
+                >
+                  <Col
+                    debug
+                    style={{
+                      outline: "none",
+                      background: "white",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <ProjectCard
+                      img={punggolWalk}
+                      name="308B Punggol Walk"
+                      link="/"
+                    ></ProjectCard>
+                  </Col>
+                </Row>
+              </div>
+            )}
+          </Container>
         </div>
       </div>
       <Footer />
