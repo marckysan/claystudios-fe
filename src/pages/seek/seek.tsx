fff0aa149dc98183bc2064753065a6808303a160
natashasoon <natashasoon@u.nus.edu>
2021-12-23 00:34:05 +0800

    feat(seek): added animations

--- src/pages/seek/seek.tsx
+++ src/pages/seek/seek.tsx
@@ -1,30 +1,70 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect, useState } from "react";
 import Footer from "../../components/footer/footer";
 import Header from "../../components/header/header";
-import seekImage from "../../assets/images/seekImage.png";
 import seekTopImage from "../../assets/images/seekTopImage.svg";
 import seekBottomImage from "../../assets/images/seekBottomImage.svg";
 import "./seek.css";
 
 const Seek: FC = () => {
-  useEffect(() => {
-    document.title = "seek";
-  }, []);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const seekSectionDiv = {
-    width: "100%",
-    height: "100%",
-    backgroundImage: `url(${seekImage})`,
-    "text-decoration": "none",
-  };
-  return (
-    <div style={{ overflowY: "hidden" }}>
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+      console.log(isLoading);
+    }, 3200);
+  }, [isLoading]);
+
+  return isLoading ? (
+    <div style={{ width: "100%", height: "100%", overflow: "hidden" }}>
+      <Header />
+      <div className="homeSection">
+        <div className="yoursContainerSeek">
+          <img
+            className="yoursImageHome"
+            src={require("../../assets/images/yoursImage.png").default}
+            alt="yoursImage"
+          />
+          <p className="yoursText">yours</p>
+        </div>
+        <div className="seekContainerSeek">
+          <img
+            className="seekImageHome"
+            src={require("../../assets/images/seekImage.png").default}
+            alt="seekImage"
+          />
+          <p className="seekText">seek</p>
+        </div>
+        <div className="usContainerSeek">
+          <img
+            className="usImageHome"
+            src={require("../../assets/images/usImage.png").default}
+            alt="usImage"
+          />
+          <p className="usText">us</p>
+        </div>
+        <div className="meetContainerSeek">
+          <img
+            className="meetImageHome"
+            src={require("../../assets/images/meetImage.png").default}
+            alt="meetImage"
+          />
+          <p className="meetText">meet</p>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  ) : (
+    <div style={{ overflowY: "hidden", width: "100vw", height: "100vh" }}>
       <Header />
       <div className="centerSection">
         <div className="seekContainer">
-          <div style={seekSectionDiv}>
-            <p className="seekText">seek</p>
-          </div>
+          <img
+            className="seekImageHome"
+            src={require("../../assets/images/seekImage.png").default}
+            alt="seekImage"
+          />
+          <p className="seekText">seek</p>
         </div>
         <div className="rightSection">
           <div className="topSection">
